8cd237c296078eda4252420e837249377e9fdd53
Joe Polny <joepolny@gmail.com>
2023-05-27 12:57:03 +0300

    contracts dir

--- deploy.ts
+++ deploy.ts
@@ -1,7 +1,7 @@
 import * as algokit from '@algorandfoundation/algokit-utils';
 import algosdk from 'algosdk';
 import { readFileSync } from 'fs';
-import appSpec from './artifacts/OptInARC.json';
+import appSpec from './contracts/artifacts/OptInARC.json';
 
 const algodClient = new algosdk.Algodv2('a'.repeat(64), 'http://localhost', 4001);
 const indexerClient = new algosdk.Indexer('a'.repeat(64), 'http://localhost', 8980);
@@ -21,7 +21,7 @@ async function deploy() {
 
   const { appId } = await app.create();
 
-  const optInLsigTeal = readFileSync('./optin_lsig.teal')
+  const optInLsigTeal = readFileSync('./contracts/optin_lsig.teal')
     .toString()
     .replace('TMPL_WHITELIST_APP', appId.toString());
 
@@ -29,7 +29,7 @@ async function deploy() {
 
   const optInLsigBytes = Buffer.from(compiledOptInLsig.result, 'base64').toString('hex');
 
-  const verifierLsigTeal = readFileSync('./verifier.teal')
+  const verifierLsigTeal = readFileSync('./contracts/verifier.teal')
     .toString()
     .replace('TMPL_LSIG_PROGRAM', `0x${optInLsigBytes}`);
 
