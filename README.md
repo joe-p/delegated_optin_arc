01e68e4fd1804ea2985dcbc75eee46ddb2148358
Joe Polny <joepolny@gmail.com>
2023-05-31 06:41:02 -0400

    whitelist -> allowlist

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ The goal of this ARC is to allow asset transfers without the receiver having to 
 None of this code is tested. Right now it simply serves as a proof of concept.
 
 # Files
-[optin_lsig.teal](./contracts/optin_lsig.teal) is a logic signature intended to be signed by the receiver so that another party can opt them into assets. [app.algo.ts](./contracts/app.algo.ts) is a application that allows users to whitelist which addresses are allowed to opt them in. There is also a method that stores the signature of the lsig for a given user in a box, which is verified via [verifier.teal](./contracts/verifier.teal).
+[optin_lsig.teal](./contracts/optin_lsig.teal) is a logic signature intended to be signed by the receiver so that another party can opt them into assets. [app.algo.ts](./contracts/app.algo.ts) is a application that allows users to allowlist which addresses are allowed to opt them in. There is also a method that stores the signature of the lsig for a given user in a box, which is verified via [verifier.teal](./contracts/verifier.teal).
 
 [deploy.ts](./deploy.ts) is used to deploy the contracts on a localnetwork.
--- contracts/app.algo.ts
+++ contracts/app.algo.ts
@@ -78,14 +78,14 @@ class Master extends Contract {
   }
 }
 
-type Whitelist = {account: Address, boxID: uint16};
+type Allowlist = {account: Address, boxID: uint16};
 
 const ALLOW_ALL = 0 as uint8;
 const ALLOW_NONE = 2 as uint8;
 
 // eslint-disable-next-line no-unused-vars
-class WhitelistApp extends Contract {
-  whitelist = new BoxMap<Whitelist, Address[]>();
+class AllowlistApp extends Contract {
+  allowlist = new BoxMap<Allowlist, Address[]>();
 
   status = new BoxMap<Address, uint8>({ prefix: 'status' });
 
@@ -104,42 +104,42 @@ class WhitelistApp extends Contract {
   }
 
   /**
-   * Add address to whitelist box
+   * Add address to allowlist box
    *
-   * @param boxID - The ID of the whitelist box to add the address to
-   * @param addr - The address to add to the whitelist
+   * @param boxID - The ID of the allowlist box to add the address to
+   * @param addr - The address to add to the allowlist
    * @param payment - The payment transaction to cover the MBR change
    *
    */
-  addToWhitelist(boxID: uint16, addr: Address, payment: PayTxn): void {
+  addToAllowlist(boxID: uint16, addr: Address, payment: PayTxn): void {
     const preMBR = this.app.address.minBalance;
-    const whitelist: Whitelist = { account: this.txn.sender, boxID: boxID };
+    const allowlist: Allowlist = { account: this.txn.sender, boxID: boxID };
 
-    if (this.whitelist.exists(whitelist)) {
-      this.whitelist.get(whitelist).push(addr);
+    if (this.allowlist.exists(allowlist)) {
+      this.allowlist.get(allowlist).push(addr);
     } else {
-      const newWhitelist: Address[] = [addr];
-      this.whitelist.put(whitelist, newWhitelist);
+      const newAllowlist: Address[] = [addr];
+      this.allowlist.put(allowlist, newAllowlist);
     }
 
     this.verifyMBRPayment(payment, preMBR);
   }
 
   /**
-   * Sets a address whitelist for the sender. Should only be used when adding/removing
+   * Sets a address allowlist for the sender. Should only be used when adding/removing
    * more than one address
    *
-   * @param boxID - The ID of the whitelist box to put the addresses in
-   * @param addrs - Array of addresses that signify the whitelisted addresses
+   * @param boxID - The ID of the allowlist box to put the addresses in
+   * @param addrs - Array of addresses that signify the allowlisted addresses
    *
    */
-  setWhitelist(boxID: uint16, addrs: Address[]): void {
+  setAllowlist(boxID: uint16, addrs: Address[]): void {
     const preMBR = this.app.address.minBalance;
-    const whitelist: Whitelist = { account: this.txn.sender, boxID: boxID };
+    const allowlist: Allowlist = { account: this.txn.sender, boxID: boxID };
 
-    this.whitelist.delete(whitelist);
+    this.allowlist.delete(allowlist);
 
-    this.whitelist.put(whitelist, addrs);
+    this.allowlist.put(allowlist, addrs);
 
     if (preMBR > this.app.address.minBalance) {
       this.sendMBRPayment(preMBR);
@@ -149,33 +149,33 @@ class WhitelistApp extends Contract {
   }
 
   /**
-   * Deletes a address whitelist for the sender
+   * Deletes a address allowlist for the sender
    *
-   * @param boxID - The ID of the whitelist box to delete
+   * @param boxID - The ID of the allowlist box to delete
    *
    */
-  deleteWhitelist(boxID: uint16): void {
+  deleteAllowlist(boxID: uint16): void {
     const preMBR = this.app.address.minBalance;
-    const whitelist: Whitelist = { account: this.txn.sender, boxID: boxID };
+    const allowlist: Allowlist = { account: this.txn.sender, boxID: boxID };
 
-    this.whitelist.delete(whitelist);
+    this.allowlist.delete(allowlist);
 
     this.sendMBRPayment(preMBR);
   }
 
   /**
-   * Deletes a address from a whitelist for the sender
+   * Deletes a address from a allowlist for the sender
    *
-   * @param boxID - The ID of the whitelist box to delete from
-   * @param addr - The address to delete from the whitelist
-   * @param index - The index of the address in the whitelist
+   * @param boxID - The ID of the allowlist box to delete from
+   * @param addr - The address to delete from the allowlist
+   * @param index - The index of the address in the allowlist
    *
    */
-  deleteFromWhitelist(boxID: uint16, addr: Address, index: uint64): void {
+  deleteFromAllowlist(boxID: uint16, addr: Address, index: uint64): void {
     const preMBR = this.app.address.minBalance;
-    const whitelist: Whitelist = { account: this.txn.sender, boxID: boxID };
+    const allowlist: Allowlist = { account: this.txn.sender, boxID: boxID };
 
-    const spliced = this.whitelist.get(whitelist).splice(index, 1);
+    const spliced = this.allowlist.get(allowlist).splice(index, 1);
 
     assert(spliced[0] === addr);
 
@@ -183,10 +183,10 @@ class WhitelistApp extends Contract {
   }
 
   /**
-   * Verifies that the sender is in the whitelist
+   * Verifies that the sender is in the allowlist
    *
-   * @param boxID - The ID of the whitelist box to delete from
-   * @param index - The index of the address in the whitelist
+   * @param boxID - The ID of the allowlist box to delete from
+   * @param index - The index of the address in the allowlist
    *
    */
   verifySender(
@@ -200,20 +200,20 @@ class WhitelistApp extends Contract {
     assert(status !== ALLOW_NONE);
     if (status === ALLOW_ALL) return;
 
-    const whitelist: Whitelist = { account: optIn.assetReceiver, boxID: boxID };
+    const allowlist: Allowlist = { account: optIn.assetReceiver, boxID: boxID };
 
-    const whitelistAddr = this.whitelist.get(whitelist)[index];
-    assert(whitelistAddr === this.txn.sender);
+    const allowlistAddr = this.allowlist.get(allowlist)[index];
+    assert(allowlistAddr === this.txn.sender);
   }
 
   /**
-   * Set the status of the opt-in whitelist
+   * Set the status of the opt-in allowlist
    *
-   * @param status - 0 for allowing all opt ins, 1 for allowing only whitelisted opt ins,
+   * @param status - 0 for allowing all opt ins, 1 for allowing only allowlisted opt ins,
    * 2 for allowing no opt ins. If status doesn't exist, it is assumed to be 0
    *
    */
-  setWhitelistStatus(status: uint8): void {
+  setAllowlistStatus(status: uint8): void {
     assert(status <= ALLOW_NONE);
     this.status.put(this.txn.sender, status);
   }
